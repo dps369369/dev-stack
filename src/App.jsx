d879e2c1d43a087398e61d391dd53efc7579b358
dps369369 <debpriosarker11@gmail.com>
2026-09-13 10:35:46 +0600

    feat: build main app structure

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// Import list i
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Technologies from "./components/Technologies";
+import Footer from './components/Footer'
+import './components/Responsive.css' 
+
+//  FUNCTION
+function App() {
+  // Stores the technologies selected by the user
+  const [stack, setStack] = useState([]);
+
+  return (
+    <div>
+
+      <Navbar />
+      <Hero />
+      <Technologies
+        stack={stack}
+        setStack={setStack}
+      />
+      <Footer />
+      <ToastContainer position="bottom-right" />
+
+
+    </div>
+  );
+}
+
+export default App;
